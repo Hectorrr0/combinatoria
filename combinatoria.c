d14ec2579b8e315f75af5f4ac23da17bed21e978
Hector <thehectorr.05@gmail.com>
2024-07-18 00:01:02 -0400

    funcion para calcular el factorial

--- combinatoria.c
+++ combinatoria.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <math.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
-int main(int argc, char *argv[]) {
-	return 0;
+// Función para calcular el factorial de un número
+unsigned long long factorial(int n) {
+    unsigned long long result = 1;
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
 }
+
